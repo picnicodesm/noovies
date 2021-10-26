f6f93d4bb8c560edf49db4d58ceeb2d9439dca78
PicNic <offiksm0903@naver.com>
2021-10-26 22:49:02 +0900

    study_3.5 Refactor_Error
    (6: 56)

---
+++ components/Slide.tsx
@@ -0,0 +1,83 @@
+import { BlurView } from "expo-blur";
+import React from "react";
+import { StyleSheet, useColorScheme, View } from "react-native";
+import styled from "styled-components/native";
+import { makeImgPath } from "../utils";
+
+const BgImg = styled.Image``;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba(0, 0, 0, 0.8)"};
+`;
+const Votes = styled(Overview)`
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+interface SlideProps {
+  backdropPath: string;
+  posterPath: string;
+  originalTitle: string;
+  voteAverage: number;
+  overview: string;
+}
+
+const Slide: React.FC<SlideProps> = ({
+  backdropPath,
+  posterPath,
+  originalTitle,
+  voteAverage,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <View style={{ flex: 1 }}>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImgPath(backdropPath) }}
+      />
+      <BlurView
+        tint={isDark ? "dark" : "light"}
+        intensity={85}
+        style={StyleSheet.absoluteFill}
+      >
+        <Wrapper>
+          <Poster source={{ uri: makeImgPath(posterPath) }} />
+          <Column>
+            <Title isDark={isDark}>{originalTitle}</Title>
+            {voteAverage > 0 ? (
+              <Votes isDark={isDark}>⭐{voteAverage}/10</Votes>
+            ) : null}
+            <Overview isDark={isDark}>{overview.slice(0, 100)}...</Overview>
+          </Column>
+        </Wrapper>
+      </BlurView>
+    </View>
+  );
+};
+
+export default Slide;
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,23 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect, useState } from "react";
-import {
-  Dimensions,
-  ActivityIndicator,
-  StyleSheet,
-  useColorScheme,
-} from "react-native";
-import Swiper from "react-native-web-swiper";
+import { ActivityIndicator, Dimensions } from "react-native";
+import Swiper from "react-native-swiper";
 import styled from "styled-components/native";
-import { BlurView } from "expo-blur";
-import { makeImgPath } from "../utils";
+import Slide from "../components/Slide";
 
 const API_KEY = "254cf5e35a92db305509644d6bf10686";
 
 const Container = styled.ScrollView``;
-
-const View = styled.View`
-  flex: 1;
-`;
 
 const Loader = styled.View`
   flex: 1;
@@ -25,49 +15,16 @@ const Loader = styled.View`
   align-items: center;
 `;
 
-const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
-
-const Title = styled.Text`
-  font-size: 16px;
-  font-weight: 600;
-  color: white;
-`;
-
-const Wrapper = styled.View`
-  flex-direction: row;
-  height: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-const Column = styled.View`
-  width: 40%;
-  margin-left: 15px;
-`;
-const Overview = styled.Text`
-  margin-top: 10px;
-  color: rgba(255, 255, 255, 0.6);
-`;
-const Votes = styled(Overview)`
-  margin-top: 5px;
-  font-size: 12px;
-`;
-
-const { height: SCEEN_HEIGHT } = Dimensions.get("window");
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const isDark = useColorScheme() === "dark";
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
   const getNowPlaying = async () => {
     const { results } = await (
-      await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR
-    `)
+      await fetch(
+        `https://api.themoviedb.org/3/movie/nowPlaying?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
     ).json();
     setNowPlaying(results);
     setLoading(false);
@@ -82,34 +39,23 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   ) : (
     <Container>
       <Swiper
+        horizontal
         loop
-        timeout={3.5}
-        controlsEnabled={false}
-        containerStyle={{ width: "100%", height: SCEEN_HEIGHT / 4 }}
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
-          <View key={movie.id}>
-            <BgImg
-              style={StyleSheet.absoluteFill}
-              source={{ uri: makeImgPath(movie.backdrop_path) }}
-            />
-            <BlurView
-              tint={isDark ? "dark" : "light"}
-              intensity={85}
-              style={StyleSheet.absoluteFill}
-            >
-              <Wrapper>
-                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
-                <Column>
-                  <Title>{movie.original_title}</Title>
-                  {movie.vote_average > 0 ? (
-                    <Votes>⭐{movie.vote_average}/10</Votes>
-                  ) : null}
-                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
-                </Column>
-              </Wrapper>
-            </BlurView>
-          </View>
+          <Slide
+            key={movie.id}
+            backdropPath={movie.backdrop_path}
+            posterPath={movie.poster_path}
+            originalTitle={movie.original_title}
+            voteAverage={movie.vote_average}
+            overview={movie.overview}
+          />
         ))}
       </Swiper>
     </Container>
