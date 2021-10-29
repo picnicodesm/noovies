f35546a5092516e0c88564c6d3fb6ef3c1fcb387
PicNic <offiksm0903@naver.com>
2021-10-29 11:27:48 +0900

    study_3.5 Refactor

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -3,14 +3,9 @@ import React from "react";
 import { StyleSheet, useColorScheme, View } from "react-native";
 import styled from "styled-components/native";
 import { makeImgPath } from "../utils";
+import Poster from "./Poster";
 
 const BgImg = styled.Image``;
-
-const Poster = styled.Image`
-  width: 100px;
-  height: 160px;
-  border-radius: 5px;
-`;
 
 const Title = styled.Text<{ isDark: boolean }>`
   font-size: 16px;
@@ -66,7 +61,7 @@ const Slide: React.FC<SlideProps> = ({
         style={StyleSheet.absoluteFill}
       >
         <Wrapper>
-          <Poster source={{ uri: makeImgPath(posterPath) }} />
+          <Poster path={posterPath} />
           <Column>
             <Title isDark={isDark}>{originalTitle}</Title>
             {voteAverage > 0 ? (
--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -20,6 +20,24 @@ const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
   const [loading, setLoading] = useState(true);
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [uncoming, setUpcoming] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const getTrending = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`
+      )
+    ).json();
+    setTrending(results);
+  };
+  const getUpComing = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setUpcoming(results);
+  };
   const getNowPlaying = async () => {
     const { results } = await (
       await fetch(
@@ -27,10 +45,13 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
       )
     ).json();
     setNowPlaying(results);
+  };
+  const getData = async () => {
+    await Promise.all([getTrending(), getUpComing(), getNowPlaying()]);
     setLoading(false);
   };
   useEffect(() => {
-    getNowPlaying();
+    getData();
   }, []);
   return loading ? (
     <Loader>
@@ -43,8 +64,8 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
         loop
         autoplay
         autoplayTimeout={3.5}
-        showsButtons={true}
-        showsPagination={true}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
         {nowPlaying.map((movie) => (
