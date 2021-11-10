4db973cc79ebd106103a87cb16aab42b67356136
PicNic <offiksm0903@naver.com>
2021-11-10 17:34:46 +0900

    study_3.21 Detail Screen part One

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -58,7 +58,13 @@ const Slide: React.FC<SlideProps> = ({
   const isDark = useColorScheme() === "dark";
   const navigation = useNavigation();
   const goToDetail = () => {
-    navigation.navigate("Stack", { screen: "Detail" });
+    //@ts-ignore
+    navigation.navigate("Stack", {
+      screen: "Detail",
+      params: {
+        originalTitle,
+      },
+    });
   };
   return (
     <TouchableWithoutFeedback onPress={goToDetail}>
--- navigation/Stack.js
+++ navigation/Stack.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, useColorScheme } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Detail from "../screens/Detail";
+import { BLACK_COLOR } from "../colors";
 
 const NativeStack = createNativeStackNavigator();
 
-const Stack = () => (
-  <NativeStack.Navigator
-    screenOptions={{
-      headerBackTitleVisible: false,
-    }}
-  >
-    <NativeStack.Screen name="Detail" component={Detail} />
-  </NativeStack.Navigator>
-);
-
+const Stack = () => {
+  const isDark = useColorScheme() === "dark";
+  return (
+    <NativeStack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+        headerStyle: { backgroundColor: isDark ? BLACK_COLOR : "white" },
+        headerTitleStyle: {
+          color: isDark ? "white" : BLACK_COLOR,
+        },
+      }}
+    >
+      <NativeStack.Screen name="Detail" component={Detail} />
+    </NativeStack.Navigator>
+  );
+};
 export default Stack;
