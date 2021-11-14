363f59cf343b926ae35e71cfda16dd38c3b8e804
PicNic <offiksm0903@naver.com>
2021-11-14 14:32:30 +0900

    study_3.23 Detail Screen part Three

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,12 +1,45 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { LinearGradient } from "expo-linear-gradient";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { Dimensions, ImageBackground, StyleSheet } from "react-native";
 import styled from "styled-components/native";
-import { Movie } from "../api";
+import { Movie, TV } from "../api";
+import { BLACK_COLOR } from "../colors";
 import Poster from "../components/Poster";
+import { makeImgPath } from "../utils";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+  width: 80%;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0px 20px;
 `;
 
 type RootStackParamList = {
@@ -21,15 +54,31 @@ const Detail: React.FC<DetailScreenProps> = ({
 }) => {
   useEffect(() => {
     setOptions({
-      title:
-        "original_title" in params
-          ? params.original_title
-          : params.original_name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path) }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", BLACK_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
